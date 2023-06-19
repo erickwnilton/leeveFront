cb4a54c174ec661068746a30e046b2670b6e6d16
23eerick <erickwelberth1@hotmail.com>
2023-06-19 20:32:46 -0300

    create setup app - leeve

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+"use client"
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <ChakraProvider>
+        <body>{children}</body>
+      </ChakraProvider>
+    </html>
+  )
+}
